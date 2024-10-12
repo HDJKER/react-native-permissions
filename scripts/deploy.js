d815f398e725e964234b48ed86d4c15544f8d5ac
amiz21@qq.com <amiz21@qq.com>
2024-10-12 09:21:29 +0800

    build: 删除脚本中对harmony文件的不必要操作

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,8 +3,8 @@
  * 执行此脚本前需要进行的额外工作
  * 1、在系统环境变量中创建
  *    a.GITHUB_TOKEN: (在github个人账户中创建，具体步骤可自行查找'github token如何创建', 需要勾选的权限-repo, admin:repo_hook, gist)
- *    b.GITHUB_OWNER: (github用户名,)
- * 
+ *    b.GITHUB_OWNER: (github用户名)
+ * 2、须手动在三方库同级目录下创建tester工程，并将所在三方库拷贝到tester/harmony工程下，并完成相关配制
  */
 
 // @ts-check
@@ -43,35 +43,9 @@ const HAR_FILE_OUTPUT_PATH = `tester/harmony/${MODULE_NAME}/build/default/output
 const UNSCOPED_NPM_PACKAGE_NAME = '@react-native-oh-tpl/react-native-permissions';
 
 const GITHUB_REPOS = 'react-native-oh-library';
-const TARGET_BRANCH = 'sig'
+const TARGET_BRANCH = 'sig';
 // pr描述信息
-const RELEASE_BODY = 
-`
-<!-- 感谢您提交PR！请按照模板填写，以便审阅者可以轻松理解和评估代码变更的影响。 -->
-
-# Summary
-
-<!-- 请解释此次更改的 **动机**，以下是一些帮助您的要点： -->
-
-- 这个 PR 解决了哪些 issues？请标记这些 issues，以便合并 PR 后这些 issues 将会被自动关闭。
-- 这个功能是什么？（如果适用）
-- 您是如何实现解决方案的？
-- 这个更改影响了库的哪些部分？
-
-## Test Plan
-
-<!-- 展示代码的稳定性。例如：用来复现场景的命令输入和结果输出、测试用例的路径地址，或者附上截图和视频。 -->
-
-## Checklist
-
-<!-- 检查项, 请自行排查并打钩, 通过: [X] -->
-
-- [ ] 已经在真机设备或模拟器上测试通过
-- [ ] 已经与 Android 或 iOS 平台做过效果/功能对比
-- [ ] 已经添加了对应 API 的测试用例（如需要）
-- [ ] 已经更新了文档（如需要）
-- [ ] 更新了 JS/TS 代码 (如有)
-`
+const RELEASE_BODY = ''; // release的pr可以不绑定信息，目前暂时留白
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -152,12 +126,6 @@ function harPackageMove(answer) {
     console.log(`Couldn't find ${HAR_FILE_OUTPUT_PATH}.`);
     process.exit(1);
   }
-  fs.rmSync('./harmony', { recursive: true, force: true });
-  fs.mkdirSync('./harmony');
-  fs.renameSync(
-    `../${HAR_FILE_OUTPUT_PATH}`,
-    `./harmony/${MODULE_NAME}.har`
-  );
 }
 
 /**
